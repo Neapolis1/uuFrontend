03ad16f28a92d2956eccf3d8d92a8ea62b8befe0
Neapolis-mac <lucka.maskova54321@gmail.com>
2025-11-14 15:32:01 +0100

    0.2 - create files and start doing AppHeader

---
+++ uu-frondend/src/AppHeader.js
@@ -0,0 +1,15 @@
+import "./AppHeader.css";
+import logo from "./imgs/shopping-store.png";
+import user from "./imgs/user.png";
+
+function AppHeader() {
+  return (
+    <div className="App-header">
+        <img src={logo} alt="Shopping Store Logo" style={{ height: '40px', marginLeft: "5px" }} />
+        <h1>Shopper</h1>
+        <img src={user} alt="User Icon" style={{ height: '40px', marginRight: "5px", marginLeft: "auto" }} />
+    </div>
+  );
+}
+
+export default AppHeader;
